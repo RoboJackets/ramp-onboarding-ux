7555de8d7458ed1cc8d780cf5ecb57f1cf414d66
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-07-10 11:05:13 -0400

    Check manager department in basic mode

--- ramp_onboarding_ux.py
+++ ramp_onboarding_ux.py
@@ -2024,6 +2024,11 @@ def get_ramp_user(apiary_id: str) -> Dict[str, str]:
     )
     ramp_user_response.raise_for_status()
 
+    if ramp_user_response.json()["department_id"] != app.config["RAMP_DEFAULT_DEPARTMENT_STUDENTS"]:
+        return {
+            "error": "Selected manager isn't in selected department"
+        }
+
     if ramp_user_response.json()["status"] == "USER_ACTIVE":
         return {
             "rampUserId": ramp_user_id,
