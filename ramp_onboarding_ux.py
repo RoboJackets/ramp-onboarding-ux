00c460228cb02d40776eb39825000ed0bb6dc115
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-07-17 14:30:11 -0400

    Use non-greedy operator for parsing emails

--- ramp_onboarding_ux.py
+++ ramp_onboarding_ux.py
@@ -2450,7 +2450,7 @@ def handle_postmark_inbound_event() -> Any:
         raise BadRequest("missing TextBody")
 
     results = search(
-        r"\[(?P<invitation_url>https://[a-z]+\.ramp\.com/s/app/invite-sign-up.+)\]",
+        r"\[(?P<invitation_url>https://[a-z]+\.ramp\.com/s/app/invite-sign-up.+?)\]",
         request.json["TextBody"],
     )
 
